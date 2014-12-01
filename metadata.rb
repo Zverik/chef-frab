d2a23b7a1df81eb84df42d2c77924869524bad5d
Ronny Trommer <ronny@opennms.org>
2014-12-01 10:42:47 +0100

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'ronny@opennms.org'
 license          'GNU GENERAL PUBLIC LICENSE 3'
 description      'Installs/Configures frab'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
